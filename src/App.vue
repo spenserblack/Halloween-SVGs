03daf97de65d949e68726ec2fdd5d3910cf39f9b
Spenser Black <spenserblack01@gmail.com>
2020-09-25 23:45:59 -0400

    Init project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang='pug'>
+  #app
+    p Hello, World!
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
